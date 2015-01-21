cd690bc1e4f5a2a3086dcb18a9dc7d17769e7e21
cmkoller <cmkoller01@gmail.com>
2015-01-21 15:29:55 -0500

    Delete buttons are now working

--- app/assets/javascripts/neural_net_build.js
+++ app/assets/javascripts/neural_net_build.js
@@ -19,13 +19,28 @@ $(document).ready(function() {
     $(".neural-net-layers").append(layerButtonsText);
 
     // New Node button
-    $(".node-edit-button").on("click", function(e) {
+    $(".add").on("click", function(e) {
       e.preventDefault();
+      e.stopImmediatePropagation();
 
       $(this).before(nodeText);
-
     });
 
+    // Delete Node button
+    $(".delete").on("click", function(e) {
+      e.preventDefault();
+
+      $(this).next().remove();
+    });
   });
+
+  // Delete Layer Button
+  $(".neural-net-delete-layer").on("click", function(e) {
+    e.preventDefault();
+
+    $('.layer').last().remove();
+
+  });
+
 
 });
